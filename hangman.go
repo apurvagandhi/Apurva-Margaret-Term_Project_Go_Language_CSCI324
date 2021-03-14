7e721876feddff4c838ef9c46a2d1c224ddf7f07
ahgand22 <71194410+ahgand22@users.noreply.github.com>
2021-03-14 20:43:52 +0530

    Sunday Early Morning (Apurva)

--- hangman.go
+++ hangman.go
@@ -7,72 +7,53 @@ TaskList:
 3. Using Algorithm, randomly select a node from binary search tree
 4. Make game graphical
 5. numGuessed Needs to be fixed if letter guessed more than once
+6. Delete \n from entered response.
 */
 
 package main
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
+	//"image/draw"
+
 )
 
 var guessed = make(map[int]bool)
 var wrong int = 0
 var numGuessed int = 0
-var ourList *List = nil
+var myLinkedlist = list.New()
+var t Tree
 
-type LLNode struct {
-	letterGuessed string
-	next          *LLNode
-}
 
-type List struct {
-	head *LLNode
-	tail *LLNode
-}
-
-func (L *List) isDuplicate(newLetter string) bool {
-	if L.head == nil {
-		return false
-	} else {
-		for L != nil {
-			if newLetter == L.head.letterGuessed {
-				fmt.Println("You have already guessed this letter, please try another!")
-				numGuessed--
-				return true
-			} else {
-				L.head = L.head.next
-			}
+func printLinkedList() {
+	for element := myLinkedlist.Front(); element != nil; element = element.Next() {
+		if element.Value != "\n" {
+			fmt.Print(element.Value)
 		}
-		// subtract num guessed if this letter is in the word
 	}
+}
+
+func CheckAndAddLetter(newLetter string) bool {
+	for element := myLinkedlist.Front(); element != nil; element = element.Next() {
+		if newLetter == element.Value {
+			return true
+		}
+	}
+	myLinkedlist.PushFront(newLetter)
+	fmt.Println("Guessed Letter Added Successfully to the Linked List")
 	return false
-}
-
-func (L *List) addLetter(newLetter string) {
-	newNode := &LLNode{letterGuessed: newLetter}
-	if !(L.isDuplicate(newLetter)) {
-		if L.head == nil {
-			L.head = newNode
-		} else if newNode.letterGuessed < L.head.letterGuessed {
-			newNode.next = L.head
-			L.head = newNode
-		} else {
-			current := L.head
-			for current.next != nil && current.next.letterGuessed < newNode.letterGuessed {
-				current = current.next
-			}
-			newNode.next = current.next
-			current.next = newNode
-		}
-	}
 }
 
 func displayHyphen(word string) {
 	for j := 0; j < len(word); j++ {
-		if guessed[j] == true {
+		if guessed[j] {
 			// print the letter
 			fmt.Print(string(word[j]))
 			fmt.Print(" ")
@@ -86,22 +67,56 @@ func displayHyphen(word string) {
 		fmt.Print("- ")
 	}
 	fmt.Println()
-	fmt.Print("Here are the letters you have guessed: ")
+	//fmt.Print("Here are the letters you have guessed: ")
+
+}
+func pickRandomWordFromDict() string {
+	letters, err := ioutil.ReadFile("smallDictionary.txt")
+	lettersString := string(letters)
+	words := strings.Split(lettersString, "\n")
+	totalWords := len(words) - 1
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	randnum := r1.Intn(totalWords)
+	fmt.Print("Random number is ")
+	fmt.Println(randnum)
+
+	randomWord := words[randnum]
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	return randomWord
 
 }
 
-func checkLetter(letter string, word string, L *List) {
-	L.addLetter(letter)
-	inWord := false
-	for i := 0; i < len(word); i++ {
-		if strings.ToLower(letter) == strings.ToLower(string(word[i])+"\n") {
-			guessed[i] = true
-			numGuessed++
-			inWord = true
+func checkLetter(letter string, word string) {
+	checkRepeat := CheckAndAddLetter(letter)
+	if checkRepeat {
+		fmt.Println("You have already entered this guess, try new one")
+	} else {
+
+		inWord := false
+		for i := 0; i < len(word); i++ {
+			fmt.Println("Letter is " + strings.ToLower(letter))
+			fmt.Println("letter of the word is " + string(word[i]) + "\n")
+			//fmt.Println(strings.Compare(strings.ToLower(letter), strings.ToLower(string(word[i])+"\n")))
+			fmt.Print("Letter equals letter of the word? ")
+			fmt.Println(strings.ToLower(letter) == strings.ToLower(string(word[i])+"\n"))
+			
+			if strings.ToLower(letter) == strings.ToLower(string(word[i])+"\n") {
+				//if (strings.Compare(strings.ToLower(letter), strings.ToLower(string(word[i])+"\n"))) == 0 {
+
+				fmt.Println("Check")
+				guessed[i] = true
+				numGuessed++
+				inWord = true
+			}
 		}
-	}
-	if !inWord {
-		wrong++
+
+		if !inWord {
+			wrong++
+		}
 	}
 	fmt.Println("num guessed right, num guessed wrong:")
 	fmt.Println(numGuessed, wrong)
@@ -129,7 +144,8 @@ func (t *Tree) insert(data string) {
 
 //Node
 func (n *Node) insert(data string) {
-	if data <= n.key {
+
+	if reverse(data) <= reverse(n.key) {
 		if n.left == nil {
 			n.left = &Node{key: data}
 		} else {
@@ -142,7 +158,6 @@ func (n *Node) insert(data string) {
 			n.right.insert(data)
 		}
 	}
-
 }
 
 func printPostOrder(n *Node) {
@@ -154,7 +169,39 @@ func printPostOrder(n *Node) {
 		fmt.Println(n.key)
 	}
 }
+func reverse(str string) (result string) {
+	for _, l := range str {
+		result = string(l) + result
+	}
+	return
+}
 
+func dictionaryToBinaryTree(fileName string) {
+	word, err := os.Open("smallDictionary.txt")
+	//if error, prints the error
+	if err != nil {
+		fmt.Println(err)
+	}
+	//The file descriptor is closed at the end of the main function.
+	defer word.Close()
+	// A new scanner is created.
+	scanner := bufio.NewScanner(word)
+	//split the content by words.
+	scanner.Split(bufio.ScanWords)
+	//The Scan advances the Scanner to the next token,
+	//which will then be available through the Text function.
+
+	for scanner.Scan() {
+		t.insert(scanner.Text())
+		//fmt.Println("Inserted Successfully")
+	}
+	//Checks for Error in scanning
+	scanErr := scanner.Err()
+	if err != nil {
+		fmt.Println(scanErr)
+	}
+
+}
 func searchTree(n *Node, word string) bool {
 	if n == nil {
 		return false
@@ -170,13 +217,19 @@ func searchTree(n *Node, word string) bool {
 
 func main() {
 	word := "hello"
+	randomWord := pickRandomWordFromDict()
+	fmt.Println("Random word is " + randomWord)
+	dictionaryToBinaryTree("smallDictionary.txt")
+	printPostOrder(t.root)
 	for wrong < 6 && numGuessed < len(word) {
 		displayHyphen(word)
 		fmt.Print("Please enter a letter: ")
 		reader := bufio.NewReader(os.Stdin)
 		response, _ := reader.ReadString('\n')
+		checkLetter(response, word)
+		fmt.Print("So far you entered following guesses: ")
+		printLinkedList()
 
-		checkLetter(response, word, ourList)
 	}
 	if wrong > 6 {
 		fmt.Println("Sorry, you have exceeded the maximum guesses. You Lost!")
