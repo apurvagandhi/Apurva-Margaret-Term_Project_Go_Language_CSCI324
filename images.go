9342dc7a36e4a792cf47ab86ff5e4e53de5c7323
margarethaley <mrhale22@g.holycross.edu>
2021-04-18 17:43:15 -0400

    adding images file

---
+++ images.go
@@ -0,0 +1,22 @@
+/******************************************
+Names: Apurva Gandhi and Margaret Haley
+Course: CSCI324
+Professor King
+Creative Program
+Sample execution: go run images.go
+*****************************************/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print()
+}
+
+/*
+- load in one image
+- resize and print onto a background
+*/
