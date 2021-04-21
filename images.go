0674b4883ef46373dbbccc8aae463d13a8d85820
margarethaley <mrhale22@g.holycross.edu>
2021-04-21 16:56:51 -0400

    Co-authored-by: ahgand22 <ahgand22@users.noreply.github.com>

--- images.go
+++ images.go
@@ -9,12 +9,15 @@ Sample execution: go run images.go
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
 	"image/png"
 	"os"
+	"strings"
+	"time"
 )
 
 type circle struct {
@@ -38,51 +41,83 @@ func (c *circle) At(x, y int) color.Color {
 	return color.Alpha{0}
 }
 
+// start() gets the images the user wants to put in their collage, and places their names in an array
+func getNames() []string {
+	fmt.Println("Please enter the names of fours images separated by a space (png only). For example: ")
+	fmt.Println("image1.png image2.png image3.png image4.png")
+	reader := bufio.NewReader(os.Stdin)
+	response, _ := reader.ReadString('\n')
+	image_names := strings.Split(response, " ")
+	for len(image_names) != 4 {
+		fmt.Println("Sorry, you have to input 4 names. Please try again.")
+		reader := bufio.NewReader(os.Stdin)
+		response, _ := reader.ReadString('\n')
+		image_names = strings.Split(response, " ")
+	}
+	// Remove the extra carriage return from the last element of the array
+	changeName := image_names[len(image_names)-1]
+	image_names[len(image_names)-1] = ""
+	for i := 0; i < len(changeName)-1; i++ {
+		image_names[len(image_names)-1] += string(changeName[i])
+	}
+	return image_names
+}
+
 func main() {
-	// input an image
-	picture1, err := os.Open("covid2.png")
-	if err != nil {
-		fmt.Println("Sadly, an error has occured with this image: ", err)
-	}
-	defer picture1.Close()
 
-	theImage, format, err := image.Decode(picture1)
-	if err != nil {
-		fmt.Println("Sadly, an error has occured with this image: ", err)
-	}
-	fmt.Println("format is: ", format)
+	// Get names of images from user
+	image_names := getNames()
+	fmt.Println(image_names)
 
-	// convert to RGBA
-	bounds := theImage.Bounds()
-	m := image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
-	draw.Draw(m, m.Bounds(), theImage, bounds.Min, draw.Src)
-	//fmt.Println("bounds are: ", bounds)
+	// Start a timer
+	starting := time.Now()
 
 	// initialize new image to all black
-	background := image.NewRGBA(image.Rect(0, 0, 640, 480))
+	background := image.NewRGBA(image.Rect(0, 0, 480, 480))
 	black := color.RGBA{0, 0, 0, 255}
 	draw.Draw(background, background.Bounds(), &image.Uniform{black}, image.ZP, draw.Src)
 
-	// paste on background
-	//sr := bounds
-	//dp := image.Point{50, 50}
-	// converting source rectangle into destination's coordinates
-	//collage := image.Rectangle{dp, dp.Add(sr.Size())}
-	//draw.Draw(background, collage, theImage, sr.Min, draw.Src)
+	for i := 0; i < len(image_names); i++ {
 
-	//Paste on cropped background circle
-	draw.DrawMask(background, background.Bounds(), theImage, image.ZP, &circle{100, image.Point{100, 100}}, image.ZP, draw.Over)
+		// Input an image
+		picture, err := os.Open(image_names[i])
+		if err != nil {
+			fmt.Println("Sadly, an error has occured with this image: ", err)
+		}
+		defer picture.Close()
 
-	/*
-		func Width(i image.Image) int {
-			return i.Bounds().Max.X — i.Bounds().Min.X
-		  }
-		  func Height(i image.Image) int {
-			return i.Bounds().Max.Y — i.Bounds().Min.Y
-		  }
+		// Decode it
+		theImage, _, err := image.Decode(picture)
+		if err != nil {
+			fmt.Println("Sadly, an error has occured with this image: ", err)
+		}
 
-	*/
+		/*
+			// Convert to RGBA
+			bounds := theImage.Bounds()
+			m := image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+			draw.Draw(m, m.Bounds(), theImage, bounds.Min, draw.Src)
+		*/
 
+		// Calculate the top left and bottom right of the mini-square we're pasting to
+		var sp image.Point
+		var ep image.Point
+		if i == 0 {
+			sp = image.Point{0, 0}
+			ep = image.Point{240, 240}
+		} else if i == 1 {
+			sp = image.Point{240, 0}
+			ep = image.Point{480, 240}
+		} else if i == 2 {
+			sp = image.Point{0, 240}
+			ep = image.Point{240, 480}
+		} else {
+			sp = image.Point{240, 240}
+			ep = image.Point{480, 480}
+		}
+		// Paste a circle on the background
+		draw.DrawMask(background, image.Rectangle{sp, ep}, theImage, image.ZP, &circle{120, image.Point{120, 120}}, image.ZP, draw.Over)
+	}
 	// output the image into a file named output
 	out, err := os.Create("output.png")
 	if err != nil {
@@ -95,17 +130,14 @@ func main() {
 		fmt.Println("Sadly, an error has occured with this image: ", err)
 	}
 
-	/*
-		// Specify the quality, between 0-100
-		// Higher is better
-		opt := jpeg.Options{
-			Quality: 90,
-		}
-		err = jpeg.Encode(picture, theImage, &opt)
-		if err != nil {
-			// Handle error
-			fmt.Println("error!")
-			fmt.Println(err)
-		}*/
-
+	totaltime := time.Since(starting)
+	fmt.Printf("Making your collage took %s", totaltime)
+	fmt.Println()
 }
+
+// paste on background
+//sr := bounds
+//dp := image.Point{50, 50}
+// converting source rectangle into destination's coordinates
+//collage := image.Rectangle{dp, dp.Add(sr.Size())}
+//draw.Draw(background, collage, theImage, sr.Min, draw.Src)
